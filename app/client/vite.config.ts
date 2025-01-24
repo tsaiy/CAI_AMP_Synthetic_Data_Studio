e7174fb110aef2267541714a8b656977bdf20794
László Kiss <kisslac@cloudera.com>
2025-01-24 18:32:47 +0100

    fix env variable cases

--- app/client/vite.config.ts
+++ app/client/vite.config.ts
@@ -10,9 +10,9 @@ const prodEnvVar = {
   'import.meta.env.VITE_HF_TOKEN': JSON.stringify(process.env.hf_token),
   'import.meta.env.VITE_HF_USERNAME': JSON.stringify(process.env.hf_username),
   'import.meta.env.VITE_AMP_URL': JSON.stringify(''),
-  'import.meta.env.VITE_CDSW_API_URL': JSON.stringify(process.env.cdsw_api_url),
-  'import.meta.env.VITE_CDSW_DOMAIN': JSON.stringify(process.env.cdsw_domain),
-  'import.meta.env.VITE_CDSW_APIV2_KEY': JSON.stringify(process.env.cdsw_apiv2_key),
+  'import.meta.env.VITE_CDSW_API_URL': JSON.stringify(process.env.CDSW_API_URL),
+  'import.meta.env.VITE_CDSW_DOMAIN': JSON.stringify(process.env.CDSW_DOMAIN),
+  'import.meta.env.VITE_CDSW_APIV2_KEY': JSON.stringify(process.env.CDSW_APIV2_KEY),
 }
 
 export default defineConfig(({ mode }) => {
