55df312b38780fd9b1cee07f0a80cf8d63cc2e2a
Keivan Vosoughi <kvosoughi@cloudera.com>
2025-02-06 11:18:46 -0800

    Dataset Details Page Add Generation Table Add Dataset Details Page Add Upgrade Button & Check Status Mechanism Add Custom Prompt to the Deatils Page Add Evaluation Details Fix for brand title color Testing Upgrade Fix Custom Prompt for Evaluator Fix for Fetch Model Parameters Adding Error Messages for Evaluate & Re-evaluate Adjust Upgrade Look & Feel Upgrade Loading Fix for Upgrade Message

---
+++ app/client/src/pages/DatasetDetails/CustomGenerationTable.tsx
@@ -0,0 +1,91 @@
+import React, { SyntheticEvent, useEffect } from 'react';
+
+import { Col, Input, Row, Table } from 'antd';
+import { CustomResult } from '../DataGenerator/types';
+import { DatasetGeneration } from '../Home/types';
+import { sortItemsByKey } from '../../utils/sortutils';
+import { SearchProps } from 'antd/es/input/Search';
+import { throttle } from 'lodash';
+
+const { Search } = Input;
+
+interface Props {
+    results: DatasetGeneration[]
+}
+
+
+const CustomGenerationTable: React.FC<Props> = ({ results }) => {
+    console.log('CustomGenerationTable > results', results);
+    const [searchQuery, setSearchQuery] = React.useState<string | null>(null);
+    const [filteredResults, setFilteredResults] = React.useState<DatasetGeneration[]>(results || []);
+
+    useEffect(() => {
+        if (searchQuery) {
+            const filtered = results.filter((result: DatasetGeneration) => {
+                // clean up the filter logic
+                return result?.Prompt?.toLowerCase().includes(searchQuery.toLowerCase()) || result?.Completion?.toLowerCase().includes(searchQuery.toLowerCase());
+            });
+            setFilteredResults(filtered);
+        } else {
+            setFilteredResults(results);
+        }
+    }, [results, searchQuery]);
+
+    const columns = [
+        {
+            title: 'Prompt',
+            ellipsis: true,
+            sorter: sortItemsByKey('Prompt'),
+            render: (record: DatasetGeneration) => {
+                const question = record?.question || record?.Prompt || record?.prompt;
+                return <>{question}</>;
+            }
+        },
+        {
+            title: 'Completion',
+            ellipsis: true,
+            sorter: sortItemsByKey('Completion'),
+            render: (record: DatasetGeneration) => {
+                const solution = record?.solution || record?.Completion || record?.completion;
+                return <>{solution}</>;
+            }
+        }
+    ];
+
+     const onSearch: SearchProps['onSearch'] = (value, _e, info) => {
+        throttle((value: string) => setSearchQuery(value), 500)(value);
+    }
+        
+    const onChange = (event: SyntheticEvent) => {
+        const value = event.target?.value;
+        throttle((value: string) => setSearchQuery(value), 500)(value);
+    }
+
+    return (
+        <>
+            <Row style={{ marginBottom: '16px', marginTop: '16px' }}>
+                <Col span={24}>
+                    <Search
+                        placeholder="Search Datasets"
+                        onSearch={onSearch}
+                        onChange={onChange}
+                        style={{ width: 350 }} />
+                </Col>
+            </Row>
+            <Table
+                columns={columns}
+                dataSource={filteredResults}
+                rowClassName={() => 'hover-pointer'}
+                rowKey={(_record, index) => `generation-table-${index}`}
+                pagination={{
+                    showSizeChanger: true,
+                    showQuickJumper: false,
+                    hideOnSinglePage: true
+                }}
+            />
+        </>
+        
+    )
+}
+
+export default CustomGenerationTable;
---
+++ app/client/src/pages/DatasetDetails/ExamplesSection.tsx
@@ -0,0 +1,151 @@
+import { Collapse, Descriptions, Flex, Modal, Table, Typography } from "antd";
+import styled from "styled-components";
+import Markdown from "../../Markdown";
+import { DatasetResponse } from "../../../api/Datasets/response";
+import { QuestionSolution } from "../../../pages/DataGenerator/types";
+import { MODEL_PARAMETER_LABELS, ModelParameters, Usecases } from "../../../types";
+import { Dataset } from "../../../pages/Evaluator/types";
+import PCModalContent from "../../../pages/DataGenerator/PCModalContent";
+
+import ExampleModal from "./ExampleModal";
+
+const { Text, Title } = Typography;
+const Panel = Collapse.Panel;
+
+
+const StyledTable = styled(Table)`
+  font-family: Roboto, -apple-system, 'Segoe UI', sans-serif;
+  color:  #5a656d;
+  .ant-table-thead > tr > th {
+    color: #5a656d;
+    border-bottom: 1px solid #eaebec;
+    font-weight: 500;
+    text-align: left;
+    // background: #ffffff;
+    border-bottom: 1px solid #eaebec;
+    transition: background 0.3s ease; 
+  }
+    .ant-table-row {
+        cursor: pointer;
+    }
+  .ant-table-row > td.ant-table-cell {
+    padding: 8px;
+    padding-left: 16px;
+    font-size: 13px;
+    font-family: Roboto, -apple-system, 'Segoe UI', sans-serif;
+    color:  #5a656d;
+    .ant-typography {
+      font-size: 13px;
+      font-family: Roboto, -apple-system, 'Segoe UI', sans-serif;
+    }
+  }
+`;
+
+const MarkdownWrapper = styled.div`
+    border: 1px solid #d9d9d9;
+    border-radius: 6px;
+    padding: 4px 11px;
+`;
+
+const StyledLabel = styled.div`
+  font-size: 16px;
+  padding-top: 8px;
+`;
+
+const StyledCollapse = styled(Collapse)`
+  .ant-collapse-content > .ant-collapse-content-box {
+    padding: 0;
+  }
+  .ant-collapse-item > .ant-collapse-header .ant-collapse-expand-icon {
+    height: 28px;
+    display: flex;
+    align-items: center;
+    padding-inline-end: 12px;
+  }  
+`;
+
+const Label = styled.div` 
+    font-size: 18px;
+    padding-top: 8px;
+`;
+
+export type DatasetDetailProps = {
+    datasetDetails: DatasetResponse | Dataset;
+}
+
+const ExamplesSection= ({ datasetDetails }: DatasetDetailProps)  => {
+
+    const exampleCols = [
+        {
+          title: 'Prompts',
+          dataIndex: 'prompts',
+          ellipsis: true,
+          render: (_text: QuestionSolution, record: QuestionSolution) => <>{record.question}</>
+        },
+        {
+          title: 'Completions',
+          dataIndex: 'completions',
+          ellipsis: true,
+          render: (_text: QuestionSolution, record: QuestionSolution) => <>{record.solution}</>
+        },
+      ]
+
+    return (
+     
+        <StyledCollapse ghost style={{ marginLeft: '-1em' }}>
+            <Panel
+                key="exmaples"
+                header={<Label>Examples</Label>}
+                style={{ padding: 0 }}
+            >        
+                <Flex vertical gap="middle">
+                    <StyledTable
+                        bordered
+                        columns={exampleCols}
+                        dataSource={datasetDetails.examples || []}
+                        pagination={false}
+                        onRow={(record: { question: string, solution: string}) => ({
+                        onClick: () => Modal.info({
+                            title: 'View Details',
+                            content: <ExampleModal {...record} />,
+                            icon: undefined,
+                            maskClosable: false,
+                            width: 1000
+                        })
+                    })}
+                    rowKey={(_record, index) => `summary-examples-table-${index}`}
+                />
+
+            {/* <Title level={4}>Model Parameters</Title>
+            <Descriptions
+                bordered
+                colon
+                column={1}
+                size='small'
+                style={{ maxWidth: 400 }}
+                items={
+                    datasetDetails?.model_parameters
+                        ? Object
+                            .keys(datasetDetails.model_parameters)
+                            .map(modelParameterKey => ({
+                                label: MODEL_PARAMETER_LABELS[modelParameterKey as ModelParameters],
+                                children: datasetDetails.model_parameters[modelParameterKey as ModelParameters],
+                            }))
+                        : []}></Descriptions>
+
+            {(datasetDetails.schema && datasetDetails.use_case === Usecases.TEXT2SQL) && (
+                <div>
+                    <Title level={4}>{'DB Schema'}</Title>
+                    <MarkdownWrapper>
+                        <Markdown text={datasetDetails.schema} />
+                    </MarkdownWrapper>
+                </div>
+            )} */}
+
+            </Flex>
+            </Panel>
+        </StyledCollapse>
+    )
+}
+
+export default ExamplesSection;
